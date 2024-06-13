d9e1fdd85d2fc242233337c7cc8a92fbf1a75fd5
Rafe Rosen <rafe@existentialmutt.com>
2024-06-12 21:55:24 -0400

    Initial commit

---
+++ lib/scrimmage/rewards.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rewards/version"
+
+module Scrimmage
+  module Rewards
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
